bc22ef84bdb4612bae24a94dd2961ac09773c328
j-Riv <jose.alfredo.rivera@gmail.com>
2025-05-27 16:41:20 -0700

    refactor: Moved Shopify Request to AWS Lambda.

--- src/RESTLets/SP_SalesRepUpdate.ts
+++ src/RESTLets/SP_SalesRepUpdate.ts
@@ -7,17 +7,75 @@ import { EntryPoints } from 'N/types';
 import * as https from 'N/https';
 import * as runtime from 'N/runtime';
 import * as log from 'N/log';
+import * as crypto from 'N/crypto';
+import * as encode from 'N/encode';
 
 type ContextType = {
   customerEmail: string;
   salesRep: string;
 };
 
-type ShopifyCustomer = {
-  id: string;
-  email: string;
-  tags: string[];
-};
+async function createSignature(body: any) {
+  const apiKeyId = runtime.getCurrentScript().getParameter({
+    name: 'custscript_sp_sales_rep_secret_id',
+  }) as string;
+
+  // create hmac
+  const secretKey = crypto.createSecretKey({
+    guid: apiKeyId,
+    encoding: crypto.Encoding.UTF_8,
+  });
+
+  log.debug({
+    title: 'Secret Key',
+    details: secretKey,
+  });
+
+  const hmac = crypto.createHmac({
+    algorithm: crypto.HashAlg.SHA256,
+    key: secretKey,
+  });
+
+  hmac.update({
+    input: JSON.stringify(body),
+    inputEncoding: encode.Encoding.UTF_8,
+  });
+
+  const digest = hmac.digest({
+    outputEncoding: encode.Encoding.BASE_64,
+  });
+
+  log.debug({
+    title: 'HMAC Signature',
+    details: digest,
+  });
+
+  return digest;
+}
+
+async function updateSalesRep(email: string, salesRep: string) {
+  const endpoint = runtime.getCurrentScript().getParameter({
+    name: 'custscript_sp_sales_rep_endpoint',
+  }) as string;
+
+  const data = {
+    email,
+    salesRep,
+  };
+
+  const signature = await createSignature(data);
+
+  const response = await https.post.promise({
+    url: endpoint,
+    body: JSON.stringify(data),
+    headers: {
+      'Content-Type': 'application/json',
+      'X-Suavecito-Hmac-Sha256': signature,
+    },
+  });
+
+  return response;
+}
 
 export const post: EntryPoints.RESTlet.post = async (context: ContextType) => {
   log.debug({
@@ -44,10 +102,11 @@ export const post: EntryPoints.RESTlet.post = async (context: ContextType) => {
   // send request
   try {
     const response = await updateSalesRep(customerEmail, salesRep);
-    return {
-      statusCode: 200,
-      body: response,
-    };
+    log.debug({
+      title: 'RESPONSE FROM SERVER (LAMBDA)',
+      details: response,
+    });
+    return response;
   } catch (err: any) {
     log.error({
       title: 'ERROR UPDATING SALES REP',
@@ -59,310 +118,3 @@ export const post: EntryPoints.RESTlet.post = async (context: ContextType) => {
     };
   }
 };
-
-function handleize(str: string) {
-  return str
-    .toLowerCase()
-    .replace(/[^a-z0-9]+/g, '-')
-    .replace(/-$/, '')
-    .replace(/^-/, '');
-}
-
-async function shopifyAuthenticatedFetch(
-  query: string,
-  variables?: { [key: string]: any }
-) {
-  // TODO: move these to params
-  const SHOPIFY_ADMIN_TOKEN = runtime
-    .getCurrentScript()
-    .getParameter({ name: 'custscript_sp_partners_admin_token' });
-
-  const SHOPIFY_DOMAIN = runtime
-    .getCurrentScript()
-    .getParameter({ name: 'custscript_sp_partners_store_name' });
-
-  const API_VERSION = runtime
-    .getCurrentScript()
-    .getParameter({ name: 'custscript_sp_partners_store_api_version' });
-
-  const ADMIN_API_ENDPOINT = `https://${SHOPIFY_DOMAIN}.myshopify.com/admin/api/${API_VERSION}/graphql.json`;
-
-  const response = await https.post.promise({
-    url: ADMIN_API_ENDPOINT,
-    body: JSON.stringify({ query, variables }),
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Shopify-Access-Token': SHOPIFY_ADMIN_TOKEN,
-    },
-  });
-
-  return response;
-}
-
-// admin api fetch
-async function getCustomer(email: string): Promise<ShopifyCustomer | null> {
-  const query = `#graphql
-    query Customers($email: String!) {
-      customers(first: 1, query: $email) {
-        edges {
-          node {
-            id
-            email
-            tags
-          }
-        }
-      }
-    }
-  `;
-
-  const variables = {
-    email,
-  };
-
-  try {
-    const response = await shopifyAuthenticatedFetch(query, variables);
-
-    const responseBody = JSON.parse(response.body);
-    log.debug({
-      title: 'RESPONSE BODY',
-      details: responseBody,
-    });
-
-    const { data, errors } = responseBody;
-
-    if (errors && errors.length > 0) {
-      throw new Error(errors);
-    }
-
-    log.debug({ title: 'CUSTOMER', details: data });
-
-    // return flat
-    log.debug({
-      title: 'CUSTOMER NODE',
-      details: data?.customers?.edges[0],
-    });
-
-    return data?.customers?.edges[0] ? data?.customers?.edges[0].node : null;
-  } catch (err: any) {
-    log.error({ title: 'ERROR', details: err.message });
-    throw new Error(err.message);
-  }
-}
-
-// admin api fetch
-async function getSalesReps() {
-  const query = `#graphql
-    query {
-      metaobjects(type: "sales_rep", first: 25) {
-        edges {
-          node {
-            id
-            handle
-            email: field(key: "email") {
-              value
-            }
-            image: field(key: "image") {
-              value
-              reference {
-                ... on MediaImage {
-                  id
-                  image {
-                    url
-                    width
-                    height
-                  }
-                }
-              }
-            }
-            name: field(key: "name") {
-              value
-            }
-            phone: field(key: "phone") {
-              value
-            }
-            extension: field(key: "phone_extension") {
-              value
-            }
-          }
-        }
-      }
-    }
-  `;
-
-  try {
-    const response = await shopifyAuthenticatedFetch(query);
-
-    const { data, errors } = JSON.parse(response.body);
-
-    if (errors && errors.length > 0) {
-      throw new Error(errors);
-    }
-    log.debug({ title: 'SALES REPS OBJECTS', details: data.metaobjects });
-    // return flat
-    return data.metaobjects.edges.map(el => el.node);
-  } catch (err: any) {
-    log.error({
-      title: 'ERROR GETTING SALES REPS (META OBJECT)',
-      details: err,
-    });
-    throw new Error(err.message);
-  }
-}
-
-const removeTags = async (customerId: string, tags: string[]) => {
-  const query = `#graphql
-    mutation RemoveTags($tags: [String!]!, $customerId: ID!) {
-      tagsRemove(tags: $tags, id: $customerId) {
-        node {
-          id
-        }
-        userErrors {
-          field
-          message
-        }
-      }
-    }
-  `;
-
-  const variables = {
-    tags,
-    customerId,
-  };
-
-  try {
-    const response = await shopifyAuthenticatedFetch(query, variables);
-    return response;
-  } catch (err: any) {
-    log.error({
-      title: 'ERROR REMOVING TAGS',
-      details: err.message,
-    });
-    throw new Error(err.message);
-  }
-};
-
-type SalesRepType = {
-  id: string;
-  handle: string;
-  email: {
-    value: string;
-  };
-  image: {
-    value: string;
-    reference: {
-      id: string;
-      image: {
-        url: string;
-        width: number;
-        height: number;
-      };
-    };
-  };
-  phone: {
-    value: string;
-  };
-  extension?: {
-    value: string;
-  };
-};
-
-// admin api
-async function updateSalesRep(customerEmail: string, rep: string) {
-  const match = rep;
-  // admin api: get customer by email
-  const customer = await getCustomer(customerEmail);
-
-  // no customer found return error
-  if (!customer) {
-    return {
-      statusCode: 500,
-      body: 'No Customer Found!',
-    };
-  }
-
-  // get tag to remove if any
-  const tags = customer.tags;
-  // find tag with sales_rep
-  const salesRepTagToRemove = tags.filter(tag => tag.includes('sales_rep:'));
-
-  log.debug({
-    title: 'SALES REP TAG TO REMOVE',
-    details: salesRepTagToRemove,
-  });
-
-  if (salesRepTagToRemove.length > 0) {
-    // remove sales rep tag
-    await removeTags(customer.id, salesRepTagToRemove);
-  }
-
-  const salesReps = await getSalesReps();
-
-  // set default sales rep
-  let salesRep = salesReps.find(
-    (rep: SalesRepType) => rep.handle === 'onboarding'
-  );
-  // // storefront api: get meta objects type sales_rep
-  // find matching sales rep
-  const found = salesReps.find(
-    (rep: SalesRepType) => rep.email.value === match
-  );
-  if (found) {
-    salesRep = found;
-  }
-  // // admin api: mutation - update sales rep
-  const query = `#graphql
-      mutation UpdateSalesRep($metafields: [MetafieldsSetInput!]!, $customerId: ID!, $tags: [String!]!) {
-        metafieldsSet(metafields: $metafields) {
-          metafields {
-            key
-            namespace
-            value
-            createdAt
-            updatedAt
-          }
-          userErrors {
-            field
-            message
-            code
-          }
-        }
-        tagsAdd(tags: $tags, id: $customerId) {
-          node {
-            id
-          }
-          userErrors {
-            field
-            message
-          }
-        }
-      }
-    `;
-
-  const variables = {
-    metafields: [
-      {
-        key: 'sales_rep',
-        namespace: 'suavecito',
-        ownerId: customer.id,
-        type: 'metaobject_reference',
-        value: salesRep.id,
-      },
-    ],
-    customerId: customer.id,
-    tags: [salesRep.name.value, `sales_rep:${salesRep.name.value}`],
-  };
-
-  try {
-    const response = await shopifyAuthenticatedFetch(query, variables);
-
-    const json = JSON.parse(response.body);
-    return json;
-  } catch (err: any) {
-    console.log('Error updating sales rep.', err);
-    log.error({
-      title: 'ERROR UPDATING SALES REP',
-      details: err.message,
-    });
-    throw new Error(err.message as string);
-  }
-}
--- src/User Events/customer_ue.ts
+++ src/User Events/customer_ue.ts
@@ -8,10 +8,14 @@ import { EntryPoints } from 'N/types';
 import * as https from 'N/https';
 import * as log from 'N/log';
 import * as runtime from 'N/runtime';
+import * as search from 'N/search';
 
 export const afterSubmit: EntryPoints.UserEvent.afterSubmit = (
   context: EntryPoints.UserEvent.afterSubmitContext
 ) => {
+  // TODO: move these to script params
+  const ACTIVE_SALES_REPS = [262, 238, 239, 243, 148066, 8703396, 206, 254];
+
   const RESTLET_SCRIPT_ID = runtime
     .getCurrentScript()
     .getParameter({ name: 'custscript_sp_partners_script_id' }) as string;
@@ -20,28 +24,7 @@ export const afterSubmit: EntryPoints.UserEvent.afterSubmit = (
     .getCurrentScript()
     .getParameter({ name: 'custscript_sp_partners_deploy_id' }) as string;
 
-  // do something
   try {
-    const oldRecord = context.oldRecord;
-
-    if (!oldRecord) {
-      log.debug({
-        title: 'Could not get oldRecord',
-        details: JSON.stringify(oldRecord),
-      });
-
-      return;
-    }
-
-    const oldSalesRep = oldRecord.getText({
-      fieldId: 'salesrep',
-    });
-
-    log.debug({
-      title: 'OLD SALES REP',
-      details: oldSalesRep,
-    });
-
     const currentRecord = context.newRecord;
 
     if (!currentRecord) {
@@ -55,64 +38,87 @@ export const afterSubmit: EntryPoints.UserEvent.afterSubmit = (
 
     const customerEmail = currentRecord.getValue({
       fieldId: 'email',
-    });
+    }) as string;
 
     log.debug({
       title: 'CUSTOMER EMAIL',
       details: customerEmail,
     });
 
-    const salesRep = currentRecord.getText({
+    const salesRepId = currentRecord.getValue({
       fieldId: 'salesrep',
+    }) as string;
+
+    log.debug({
+      title: 'SALES REP VALUE',
+      details: salesRepId,
+    });
+
+    // get sales rep by id
+    const salesRepLookup = search.lookupFields({
+      type: search.Type.EMPLOYEE,
+      id: salesRepId,
+      columns: ['email', 'firstname', 'lastname', 'internalid'],
     });
 
     log.debug({
-      title: 'NEW SALES REP',
+      title: 'SALES REP LOOKUP',
+      details: salesRepLookup,
+    });
+
+    const salesRepInternalId = salesRepLookup.internalid[0].value as string;
+
+    if (!ACTIVE_SALES_REPS.includes(parseInt(salesRepInternalId))) {
+      log.debug({
+        title: 'SALES REP EXCLUDED',
+        details: salesRepLookup,
+      });
+
+      return;
+    }
+
+    // set salesRep email
+    const salesRep = salesRepLookup.email as string;
+
+    if (!salesRep) {
+      log.debug({
+        title: 'Could not get salesRep',
+        details: JSON.stringify(salesRepLookup),
+      });
+
+      return;
+    }
+
+    log.debug({
+      title: 'SALES REP',
       details: salesRep,
     });
 
-    log.debug({
-      title: 'UPDATE SALES REP',
-      details: oldSalesRep !== salesRep,
+    // fire http request here
+    const restletResponse = https.requestRestlet({
+      method: 'POST',
+      body: JSON.stringify({
+        customerEmail,
+        salesRep,
+      }),
+      deploymentId: RESTLET_DEPLOY_ID, // 2
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      // TODO: update this with RESTLet id and or pass this in as script param
+      // deployment script external url
+      scriptId: RESTLET_SCRIPT_ID, // 1718
     });
-    // update if true
-    if (oldSalesRep !== salesRep) {
-      log.debug({
-        title: 'GETTING SALES REP',
-        details: salesRep,
-      });
 
-      // fire http request here
-      const restletResponse = https.requestRestlet({
-        method: 'POST',
-        body: JSON.stringify({
-          customerEmail,
-          salesRep,
-        }),
-        deploymentId: RESTLET_DEPLOY_ID, // 2
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        // TODO: update this with RESTLet id and or pass this in as script param
-        // deployment script external url
-        scriptId: RESTLET_SCRIPT_ID, // 1718
-      });
+    log.debug({
+      title: 'RESTLET RESPONSE',
+      details: restletResponse,
+    });
 
-      log.debug({
-        title: 'RESTLET RESPONSE',
-        details: restletResponse,
-      });
-
-      log.debug({
-        title: 'RESTLET RESPONSE BODY',
-        details: JSON.parse(restletResponse.body),
-      });
-    } else {
-      log.debug({
-        title: 'NO SALES REP UPDATE NEEDED',
-        details: { oldSalesRep, salesRep },
-      });
-    }
+    log.debug({
+      title: 'RESTLET RESPONSE BODY',
+      details: JSON.parse(restletResponse.body),
+    });
   } catch (error: any) {
     log.error({
       title: 'Something went wrong',
